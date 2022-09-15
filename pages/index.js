442408637f6259591548163200c7a24be9632764
Erzengel792 <tanadol_boat@hotmail.com>
2022-09-15 14:30:28 +0700

    Done V1

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Comment from "../components/Comment";
 import PostOwner from "../components/PostOwner";
+import Reply from "../components/Reply";
 import { comments } from "../libs/comments";
 
 export default function Home() {
+  const result = comments.filter((x) => x.likeNum > 0);
   return (
     <div
       style={{ minHeight: "100vh", backgroundColor: "#18191A" }}
@@ -13,75 +15,19 @@ export default function Home() {
         style={{ maxWidth: "700px", backgroundColor: "#242526" }}
         className="mx-auto p-3 rounded rounded-3 shadow-sm"
       >
-        {/* PostOwner Example*/}
-        <div className="vstack gap-3">
-          <div className="d-flex align-items-center gap-3">
-            <img
-              src="/profileImages/handsome.jpg"
-              width="48"
-              height="48"
-              className="rounded-circle"
-              style={{ objectFit: "cover" }}
-            />
-            <span className="fw-semibold fs-5 text-white">
-              Chayanin Suatap 12345678
-            </span>
-          </div>
-          <span className="text-white">
-            Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207
-          </span>
-          <div className="d-flex align-items-center gap-1">
-            <img src="/like.svg" width={20}></img>
-            <span style={{ color: "#B0B3B8" }}>100 คน</span>
-          </div>
-          <hr className="m-0 border" />
-        </div>
-
-        {/* Comment Example */}
-        <div className="d-flex gap-2 my-2">
-          <img
-            src="/profileImages/lisa.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
+        {/*PostOwner*/}
+        <PostOwner />
+        {/*Comment*/}
+        {result.map((x, i) => (
+          <Comment
+            username={x.username}
+            userImagePath={x.userImagePath}
+            commentText={x.commentText}
+            likeNum={x.likeNum}
+            replies={x.replies}
+            key={i}
           />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#3A3B3C" }}
-          >
-            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
-              Lisa
-            </span>
-            <br />
-            <span style={{ color: "#E4E6EB" }}>จริงค่า</span>
-            <div className="d-flex align-items-center gap-1">
-              <img src="/like.svg" width={20}></img>
-              <span style={{ color: "#B0B3B8" }}>999 คน</span>
-            </div>
-          </div>
-        </div>
-
-        {/* Reply Example */}
-        <div className="d-flex gap-2 my-2 ps-5">
-          <img
-            src="/profileImages/puppy.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
-          />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#3A3B3C" }}
-          >
-            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
-              หมาน้อย
-            </span>
-            <br />
-            <span style={{ color: "#E4E6EB" }}>จริงค้าบบบบบบบบ</span>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
